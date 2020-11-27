fc44ee2638ba0417759eb142c1482c2cfaacd694
cxw8942500 <2469199459@qq.com>
2020-11-27 22:00:53 +0800

    首页功能第五遍完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,23 @@ var baseURL = ' http://ajax.frontend.itheima.net'
 // var baseURL = ' http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (parmes) {
     parmes.url = baseURL + parmes.url
+
+    if (parmes.url.indexOf('/my/') !== -1) {
+        parmes.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    //拦截所有的ajax
+    parmes.complete = function (res) {
+        console.log(res);
+        var obj = res.responseJSON
+        if (obj.status === 1 && obj.message === "身份认证失败！") {
+            //清空本地
+            localStorage.removeItem('token')
+            //跳转页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+    //获取用户信息
+    getUserInfo()
+
+    //退出绑定事件
+    $('#btnUppLoad').on('click', function () {
+        layer.confirm('是否退出?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //清空token
+            localStorage.removeItem('token')
+            //跳转页面
+            location.href = '/login.html'
+
+            layer.close(index);
+        });
+    })
+})
+
+
+//封装一个获取用户信息的函数
+var layer = layui.layer
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            //调用一个渲染头像和信息的函数
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    //昵称优先，如果没有则用用户名
+    var name = user.nickname || user.username
+    $('#welcome').html(`欢迎 ${name}`)
+    if (user.user_pic === null) {
+        $('.layui-nav-img').hide()
+        var text = name[0].toUppCase()
+        $('.text_avatar').html(text).show()
+    } else {
+        $('.text_avatar').hide()
+        $('.layui-nav-img').attr('src', user.user_pic)
+    }
+}
